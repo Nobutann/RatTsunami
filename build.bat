eeecbd1119f5fc880b02eddddaa3c683ab57435d
Bruno <bruno.filho1000@gmail.com>
2026-05-01 18:40:52 -0300

    Final build script update: included BOSS files and includes

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ echo Compilando o jogo com %GCC_PATH%...
 if not exist libs mkdir libs
 
 :: Compila todos os arquivos .c da pasta src
-%GCC_PATH% src\main.c src\game.c src\start_menu.c src\options_menu.c src\credits_menu.c src\config_manager.c src\hitbox_menu_ini.c src\hitbox_enemy.c src\utils.c -o libs\jogo.exe -I"include" -I"C:/msys64/mingw64/include" -L"C:/msys64/mingw64/lib" -lraylib -lopengl32 -lgdi32 -lwinmm
+%GCC_PATH% src\main.c src\game.c src\start_menu.c src\options_menu.c src\credits_menu.c src\config_manager.c src\hitbox_menu_ini.c src\hitbox_enemy.c src\utils.c src\BOSS\hairy_leg\hairy_leg.c -o libs\jogo.exe -I"include" -I"src/BOSS/hairy_leg" -I"C:/msys64/mingw64/include" -L"C:/msys64/mingw64/lib" -lraylib -lopengl32 -lgdi32 -lwinmm
 
 if %errorlevel% neq 0 (
     echo.
